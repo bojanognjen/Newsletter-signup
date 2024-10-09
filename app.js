6631ed2f85a5132b927b3417de0e7ec611d18394
Bojan Lukic <bojan.ognjen@gmail.com>
2024-10-09 20:43:16 +0200

    Desktop responsive

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let errorMsg = document.querySelector('.errorMessage');
 let email = document.querySelector('#email');
 let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 let successEmail = document.querySelector('.successEmail');
+let successButton = document.querySelector('.success__button');
 
 function switchPage() {
     let main = document.querySelector('.newsletter');
@@ -12,24 +13,44 @@ function switchPage() {
 
     success.classList.toggle('hidden');
     main.classList.toggle('hidden');
+    
 }
 
 form.addEventListener('submit', handleEvent);
+successButton.addEventListener('click', ()=> {
+    switchPage();
+    location.reload();
+});
 
 function handleEvent(e) {
         e.preventDefault();
-        if(!email.value) errorMsg.innerText = "Obavezan unos mejla.";
+        if(!email.value) {
+            errorMsg.innerText = "Required";
+            email.style.borderColor = "hsl(4, 100%, 67%)";
+            email.style.backgroundColor = "hsla(4, 100%, 67%, 0.1)";
+            email.classList.add('error-placeholder');
+        }
         else if (!emailPattern.test(email.value)) {
-            errorMsg.innerText = 'Molim te unesi pravilnu mejl adresu.';
-          }
+            errorMsg.innerText = 'Valid email required';
+            email.style.borderColor = "hsl(4, 100%, 67%)";
+            email.style.backgroundColor = "hsla(4, 100%, 67%, 0.1)";
+            email.style.color = "hsl(4, 100%, 67%)";
+            }
+
+          
         else {
             errorMsg.innerText = "";
             switchPage();
             email.value = "";
-    
         }
-}
+    }
 
+   
+        email.addEventListener('input', ()=> {
+                email.style.borderColor = "#aaaaaa";
+                email.style.backgroundColor = "#fff";
+                email.style.color = "#000";
+            });
 
 
 
